003cd51fc9386d22858e5fd465dcd1d3bcafabe5
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-04-29 17:45:43 +0200

    Disables body scroll on opened modal or mob menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,11 +2,13 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
+  const body = document.body;
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
+    body.classList.toggle("disabled-scroll");
 
     const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
     bodyScrollLock[scrollLockMethod](document.body);
